18accf92abb2da3609db1d275344ffb7ab38b709
Aditya-singh101 <vishusingh212301@gmail.com>
2025-11-02 12:19:03 +0530

    Streaming inserted

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -29,3 +29,14 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+
+# For demonstration of stream: the code is applied in frontend part
+
+# for message_chunk, metadat in  chatbot.stream(
+#     {'messages' : [HumanMessage(content='What is the recipe to make pasta?')]},
+#     config = {'configurable' : {'thread_id' : 'thread-1'}},
+#     stream_mode= 'messages'
+# ) :
+#     if message_chunk.content:
+#         print(message_chunk.content, end = " ", flush=True)
--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -14,6 +14,16 @@ if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
 
+# ****************** SIDEBAR UI *****************************
+
+st.sidebar.title('Langgraph Chatbot')
+
+st.sidebar.button('New Chat')
+
+st.sidebar.header('My Conversations')
+
+# ***************** MAIN UI ***********************************
+
 # Loading the conversation history
 for message in st.session_state['message_history'] :
     with st.chat_message(message['role']):
@@ -30,13 +40,23 @@ if user_input:
         st.text(user_input)
 
 
-    response = chatbot.invoke({'messages' : [HumanMessage(content = user_input)]}, config = CONFIG)
-    ai_message = response['messages'][-1].content
+    # response = chatbot.invoke({'messages' : [HumanMessage(content = user_input)]}, config = CONFIG)
+    # ai_message = response['messages'][-1].content
     # First add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content' : ai_message})
+    # st.session_state['message_history'].append({'role': 'assistant', 'content' : ai_message})
+
+    # Now we will do stream messages not invoke
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages' : [HumanMessage(content=user_input)]},
+                config = {'configurable' : {'thread_id' : 'thread-1'}},
+                stream_mode= 'messages'
+                )
+            )
     
+    # Store in session
+    st.session_state['message_history'].append({'role': 'assistant', 'content' : ai_message})
 
 
     
